5b8d005a058e99ecb51008d0eb3fb0dd79d53aa3
vidyashreehipparagi <hipparagividyashree14@gmail.com>
2023-06-23 14:34:26 +0530

    Add project files.

---
+++ Properties2/Employee.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties2
+{
+    internal class Employee
+    {
+       //id name sal city default==>pune
+       private int id ;
+        private string name;
+        private double sal;
+        private string city;
+       
+
+        public Employee(int id, string name, double sal,string city="Pune")
+        {
+            this.id = id;
+            this.name = name;
+            this.sal = sal;
+         
+        }
+        public string PrintEmployee()
+        {
+            return $"id={id}\nname={name}\nSalary={sal}\nCity={city}";
+
+        }
+
+    }
+}
---
+++ Properties2/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee(01, "Vidya", 23000);
+            Console.WriteLine(emp.PrintEmployee());
+
+            Employee emp1 = new Employee(02, "Shruti", 23330, "Mumbai");
+          
+
+            Employee emp2 = new Employee(id: 202, name: "shruti", sal: 23302, city: "Kolhapur");
+        }
+    }
+}
